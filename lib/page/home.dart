03d928ae0f35e605ea5180d0b17df1c2926431b4
a5chin <t319080@m.chukyo-u.ac.jp>
2021-08-30 16:43:30 +0900

    add carousel

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:kairanban/components/carousel.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: Carousel(),
+        ),
+      ]
     );
   }
 }
