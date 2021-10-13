ac96d18b52f6d1f097fc30bf907009067c8c0952
Yuya-Uchikawa <yuuya_uchikawa@icloud.com>
2021-10-13 16:48:30 +0900

    detail dev #1

--- lib/page/topics/3.dart
+++ lib/page/topics/3.dart
@@ -7,18 +7,23 @@ class Topic3Page extends StatelessWidget {
         appBar: AppBar(
           title: Text('ゴミ分別方法の変更についてのご連絡'),
         ),
-        body: Column(
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Center(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        child: Image.asset('assets/images/dummy.jpg'),
-                      )
-                  )
-              ),
-            ]
+        body: ListView(
+          children: <Widget>[
+            Text('期限: 2021年12月24日18:00'),
+            Text('申請方法: 添付資料に記載'),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      child: Image.asset('assets/images/dummy.jpg'),
+                    )
+                )
+            ),
+            Text('内容: 各世帯主に一律10万円の給付'),
+            Text('備考: '),
+          ],
+
         )
     );
   }
