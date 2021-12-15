92e8aefd2e60d70a8c30cf9091eecd5672f2c19c
a5chin <81167661+a5chin@users.noreply.github.com>
2021-12-15 15:40:37 +0900

    Develop (#14)
    * create kairanban app
    
    * add NavBar (#2)
    
    * Feature home (#4)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * Feature home (#5)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * add link
    
    * Feature home hara (#7)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * add link
    
    * create branch
    
    * Feature home uchikawa (#6)
    
    * create kairanban app
    
    * add NavBar
    
    * add NavBar (#2)
    
    * add carousel
    
    * add hero
    
    * Feature home (#4)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * add link
    
    * UI作成
    
    * modified UI
    
    Co-authored-by: a5chin <t319080@m.chukyo-u.ac.jp>
    Co-authored-by: a5chin <81167661+a5chin@users.noreply.github.com>
    
    Co-authored-by: Yuya-Uchikawa <63756117+Yuya-Uchikawa@users.noreply.github.com>
    
    * Feature home uchikawa (#9)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * add link
    
    * UI作成
    
    * modified UI
    
    * hero modified.
    
    * hero modified #2
    
    * detail dev #1
    
    Co-authored-by: a5chin <t319080@m.chukyo-u.ac.jp>
    Co-authored-by: a5chin <81167661+a5chin@users.noreply.github.com>
    
    * add child (#10)
    
    * add child (#11)
    
    * add child (#12)
    
    * Feature home uchikawa (#13)
    
    * add NavBar
    
    * add carousel
    
    * add hero
    
    * add link
    
    * UI作成
    
    * modified UI
    
    * hero modified.
    
    * hero modified #2
    
    * detail dev #1
    
    Co-authored-by: Yuya-Uchikawa <yuuya_uchikawa@icloud.com>
    Co-authored-by: Yuya-Uchikawa <63756117+Yuya-Uchikawa@users.noreply.github.com>
    
    Co-authored-by: Yuya-Uchikawa <63756117+Yuya-Uchikawa@users.noreply.github.com>
    Co-authored-by: Yuya-Uchikawa <yuuya_uchikawa@icloud.com>

--- lib/page/topics/2.dart
+++ lib/page/topics/2.dart
@@ -7,19 +7,22 @@ class Topic2Page extends StatelessWidget {
         appBar: AppBar(
           title: Text('コロナワクチン予約可能な病院一覧'),
         ),
-        body: Column(
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Hero(
-                      tag: 'imageHero',
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        child: Image.asset('assets/images/dummy.jpg'),
-                      )
-                  )
-              ),
-            ]
+        body: ListView(
+          children: <Widget>[
+            Text('期限: 2021年12月24日18:00'),
+            Text('申請方法: 添付資料に記載'),
+            Text('内容: 各世帯主に一律10万円の給付'),
+            Text('備考: '),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      child: Image.asset('assets/images/dummy.jpg'),
+                    )
+                )
+            ),
+          ],
         )
     );
   }
--- lib/page/topics/4.dart
+++ lib/page/topics/4.dart
@@ -7,19 +7,22 @@ class Topic4Page extends StatelessWidget {
         appBar: AppBar(
           title: Text('自治体総決算会議の出欠確認'),
         ),
-        body: Column(
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Hero(
-                      tag: 'imageHero',
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        child: Image.asset('assets/images/dummy.jpg'),
-                      )
-                  )
-              ),
-            ]
+        body: ListView(
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      child: Image.asset('assets/images/dummy.jpg'),
+                    )
+                )
+            ),
+            Text('期限: 2021年12月24日18:00'),
+            Text('申請方法: 添付資料に記載'),
+            Text('内容: 各世帯主に一律10万円の給付'),
+            Text('備考: '),
+          ],
         )
     );
   }
