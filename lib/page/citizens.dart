d3393fd589ceb889c133ed68ac39349ca7716b6e
a5chin <t319080@m.chukyo-u.ac.jp>
2022-01-12 17:41:49 +0900

    citizens page

---
+++ lib/page/citizens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Citizens extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Citizens Info'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:kairanban/page/citizens.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Card(
+      child: ListTile(
+        title: Text('市民の情報はこちらから'),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Citizens()),
+          );
+        },
+      ),
     );
   }
 }
