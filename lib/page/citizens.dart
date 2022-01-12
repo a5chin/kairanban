8d9b748f7bca655239e8d87ff606def6ed0c9fc4
a5chin <t319080@m.chukyo-u.ac.jp>
2022-01-12 17:55:30 +0900

    create citizens's page

--- lib/page/citizens.dart
+++ lib/page/citizens.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:kairanban/page/status.dart';
 
 class Citizens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const List<String> _names = ['内河友哉', '内河ひなの'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Citizens Info'),
       ),
       body: Center(
         child: Container(
-          child: Text('Hello World'),
-        ),
+            child: ListView(children: <Widget>[
+          for (var i = 0; i < _names.length; i++)
+            Card(
+              child: ListTile(
+                title: Text(_names[i]),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StatusChecker(_names[i])),
+                  );
+                },
+              ),
+            ),
+        ])),
       ),
     );
   }
