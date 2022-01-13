a5a42762cea9455eee30400ab866e12076cc5c70
a5chin <t319080@m.chukyo-u.ac.jp>
2022-01-13 21:29:58 +0900

    modified profile.dart

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -4,16 +4,20 @@ import 'package:kairanban/page/citizens.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text('市民の情報はこちらから'),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Citizens()),
-          );
-        },
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Card(
+          child: ListTile(
+            title: Text('市民の情報はこちらから'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Citizens()),
+              );
+            },
+          ),
+        ));
   }
 }
